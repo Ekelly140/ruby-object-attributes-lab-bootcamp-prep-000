74dc9771153c5cbc29b431b3bab761f5214733a2
ERIC KELLY <eakelly220@gmail.com>
2018-04-09 19:36:01 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,13 +3,15 @@ class person
   def name=(person_name)
     @name = person_name
   end 
-  def job 
+  
+  def name 
     @job
   end 
   
   def job=(person_job)
     @job = person_job
   end 
+  
   def job 
     @job
   end 
